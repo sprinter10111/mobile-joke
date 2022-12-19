626b531adb58aead930a54e925808adfd0d319ca
JarnoDierckx <jarnodierckx@gmail.com>
2022-12-19 10:16:24 +0100

    checkboxen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-//import CheckBox from '@react-native-community/checkbox';
+import CheckBox from 'expo-checkbox';
 import { useEffect, useState } from 'react';
 
 interface Joke{
@@ -29,12 +29,12 @@ export default function App() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState<Joke>();
 
-  const [nsfw,setNsfw] = useState(false);
-  const [religious,setReligious]=useState(false);
-  const [political,setPolitical]=useState(false);
-  const [racist,setRacist]=useState(false);
-  const [sexist,setSexist]=useState(false);
-  const [explicit,setExplicit]=useState(false);
+  const [nsfw,setBlacklistNsfw] = useState(false);
+  const [religious,setBlacklistReligious]=useState(false);
+  const [political,setBlacklistPolitical]=useState(false);
+  const [racist,setBlacklistRacist]=useState(false);
+  const [sexist,setBlacklistSexist]=useState(false);
+  const [explicit,setBlacklistExplicit]=useState(false);
 
   const [contains,setContains]=useState("");
   const [idMin,setIdMin]=useState("");
@@ -99,15 +99,27 @@ export default function App() {
   
   return (
     <View style={styles.container}>
-      <Text>nsfw</Text>
+      <Text>Choose what joke types to blacklist:</Text>
+      <Text>Nsfw<CheckBox disabled={false} value={nsfw} onValueChange={(x)=>setBlacklistNsfw(x)}/></Text>
       
+      <Text>Religious<CheckBox disabled={false} value={religious} onValueChange={(x)=>setBlacklistReligious(x)}/></Text>
+      
+      <Text>Political<CheckBox disabled={false} value={political} onValueChange={(x)=>setBlacklistPolitical(x)}/></Text>
+      
+      <Text>Racist<CheckBox disabled={false} value={racist} onValueChange={(x)=>setBlacklistRacist(x)}/></Text>
+      
+      <Text>Sexist<CheckBox disabled={false} value={sexist} onValueChange={(x)=>setBlacklistSexist(x)}/> </Text>
+      
+      <Text>Explicit<CheckBox disabled={false} value={explicit} onValueChange={(x)=>setBlacklistExplicit(x)}/> </Text>
+      
+
 
       <TextInput onChangeText={text => setContains(text)} placeholder="contains"/>
       <TextInput onChangeText={text => setIdMin(text)} placeholder="idMin"/>
       <TextInput onChangeText={text => setIdMax(text)} placeholder="idMax"/>
       <Button title="get joke" disabled={false} onPress={BuildString}/>
 
-       {isLoading ? <Text>Loading...</Text> : <View><Text>{grap}</Text></View>}
+       {isLoading ? <Text></Text> : <View><Text>{grap}</Text></View>}
       <StatusBar style="auto" />
     </View>
   );
